abc4dd99b48ec73207e4775437bce73768ef8c6d
Zach Drummond <68283144+zachdrummond@users.noreply.github.com>
2025-05-09 11:41:02 -0400

    Added more fields

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,44 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 // GraphQL Schema = A collection of type definitions
 const typeDefs: string = `#graphql
 type Query {
+    feed: [Post!]!
     info: String!
 }
+
+type Post {
+  id: ID!
+  content: String!
+  createdAt: String!
+  published: Boolean!
+  title: String!
+  updatedAt: String!
+}
 `;
+
+// Data
+let posts = [{
+  id: "post-1",
+  createdAt: new Date().toLocaleString(),
+  updatedAt: new Date().toLocaleString(),
+  title: "GraphQL 101",
+  content: "Introduction to GraphQL",
+  published: true,
+}];
+
 // GraphQL Resolvers = A collection of functions that fetch the data for the schema
 const resolvers = {
   Query: {
     info: () => `This is the API of my blog`,
+    feed: () => posts,
   },
+  Post: {
+    id: (parent) => parent.id,
+    createdAt: (parent) => parent.createdAt,
+    updatedAt: (parent) => parent.updatedAt,
+    title: (parent) => parent.title,
+    content: (parent) => parent.content,
+    published: (parent) => parent.published,
+  }
 };
 
 const server = new ApolloServer({
