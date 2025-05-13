a5f971cee149568f8eb4933cd2ad875d3c2467ef
Zach Drummond <68283144+zachdrummond@users.noreply.github.com>
2025-05-12 21:40:00 -0400

    Updated addPost

--- src/server.ts
+++ src/server.ts
@@ -4,21 +4,40 @@ import fs from "fs";
 import path from "path";
 
 // Data
-let posts = [{
-  id: "post-1",
-  createdAt: new Date().toLocaleString(),
-  updatedAt: new Date().toLocaleString(),
-  title: "GraphQL 101",
-  content: "Introduction to GraphQL",
-  published: true,
-}];
+let post_list = [
+  {
+    id: "post-1",
+    createdAt: new Date().toLocaleString(),
+    updatedAt: new Date().toLocaleString(),
+    title: "GraphQL 101",
+    content: "Introduction to GraphQL",
+    published: true,
+  },
+];
 
 // GraphQL Resolvers = A collection of functions that fetch the data for the schema
 // parent = Result of previous resolver execution level
 const resolvers = {
   Query: {
     info: () => `This is the API of my blog`,
-    feed: () => posts,
+    feed: () => post_list,
+  },
+  Mutation: {
+    addPost: (parent, args) => {
+      let count = post_list.length;
+      const post = {
+        id: `post-${count++}`,
+        content: args.content,
+        createdAt: new Date().toLocaleString(),
+        title: args.title,
+        published: args.published,
+      };
+
+      post_list.push(post);
+      return post;
+    },
+    deletePost: (parent, args) => {},
+    updatePost: (parent, args) => {},
   },
   Post: {
     id: (parent) => parent.id,
@@ -27,7 +46,7 @@ const resolvers = {
     title: (parent) => parent.title,
     content: (parent) => parent.content,
     published: (parent) => parent.published,
-  }
+  },
 };
 
 const server = new ApolloServer({
