e6cd5481c815eb4a30a551021898f7706de41efb
Zach Drummond <68283144+zachdrummond@users.noreply.github.com>
2025-05-12 22:15:44 -0400

    Fixing dirname issues

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import fs from "fs";
-import path from "path";
+import { dirname, join } from "path";
+import { fileURLToPath } from 'url';
 
 // Data
 let post_list = [
@@ -12,6 +13,14 @@ let post_list = [
     title: "GraphQL 101",
     content: "Introduction to GraphQL",
     published: true,
+  },
+  {
+    id: "post-2",
+    createdAt: new Date().toLocaleString(),
+    updatedAt: new Date().toLocaleString(),
+    title: "Test Title",
+    content: "Test Content",
+    published: false,
   },
 ];
 
@@ -25,32 +34,47 @@ const resolvers = {
   Mutation: {
     addPost: (parent, args) => {
       let count = post_list.length;
+      let current_date = new Date().toLocaleString();
       const post = {
         id: `post-${count++}`,
         content: args.content,
-        createdAt: new Date().toLocaleString(),
+        createdAt: current_date,
         title: args.title,
         published: args.published,
+        updatedAt: current_date,
       };
 
       post_list.push(post);
       return post;
     },
-    deletePost: (parent, args) => {},
-    updatePost: (parent, args) => {},
-  },
-  Post: {
-    id: (parent) => parent.id,
-    createdAt: (parent) => parent.createdAt,
-    updatedAt: (parent) => parent.updatedAt,
-    title: (parent) => parent.title,
-    content: (parent) => parent.content,
-    published: (parent) => parent.published,
+    deletePost: (parent, args): boolean => {
+      for (let i = 0; i < post_list.length; i++) {
+        if (post_list[i].id === args.id || post_list[i].title === args.title) {
+          post_list.splice(i, 1);
+          return true;
+        }
+      }
+      return false;
+    },
+    updatePost: (parent, args) => {
+      for (let i = 0; i < post_list.length; i++) {
+        if (post_list[i].id === args.id) {
+          if (args.title) post_list[i].title = args.title;
+          if (args.content) post_list[i].content = args.content;
+          if (args.published) post_list[i].published = args.published;
+          post_list[i].updatedAt = new Date().toLocaleString();
+        }
+        return post_list[i];
+      }
+    },
   },
 };
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const server = new ApolloServer({
-  typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf-8"),
+  typeDefs: fs.readFileSync(join(__dirname, "schema.graphql"), "utf-8"),
   resolvers,
 });
 
