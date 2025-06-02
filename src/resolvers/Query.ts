99b2325ad609f06af596a0159773f1c7d713d60a
Zach Drummond <68283144+zachdrummond@users.noreply.github.com>
2025-06-02 09:51:07 -0400

    Moved resolver functions to Query.ts

---
+++ src/resolvers/Query.ts
@@ -0,0 +1,30 @@
+function getAllPosts(parent, args, { prisma }) {
+  return prisma.post.findMany();
+}
+
+function getPosts(parent, { ids, titles, categories }, { prisma }) {
+  if (
+    (!ids && !titles && !categories) ||
+    (ids?.length === 0 && titles?.length === 0 && categories?.length === 0)
+  ) {
+    throw new Error("Either ID, title, or category must be provided.");
+  }
+
+  // Convert string IDs to integers
+  const parsedIds = ids ? ids.map((id) => parseInt(id, 10)) : [];
+
+  return prisma.post.findMany({
+    where: {
+      OR: [
+        { id: { in: parsedIds } },
+        { title: { in: titles ? titles : [] } },
+        { categories: { hasSome: categories ? categories : [] } },
+      ],
+    },
+  });
+}
+
+module.exports = {
+  getAllPosts,
+  getPosts,
+};
