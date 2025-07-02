b80c9033ca08bc2d284229c912e089c068d93818
Zach Drummond <68283144+zachdrummond@users.noreply.github.com>
2025-07-02 18:00:16 -0400

    Added ApolloWrapper component

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { ApolloWrapper } from "./ApolloWrapper";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -33,7 +34,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ApolloWrapper>{children}</ApolloWrapper>
       </body>
     </html>
   );
