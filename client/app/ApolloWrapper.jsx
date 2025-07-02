273a390c42a30df1d0b9e90f30369188bd493cfd
Zach Drummond <68283144+zachdrummond@users.noreply.github.com>
2025-07-02 18:00:34 -0400

    Added ApolloWrapper component

---
+++ client/app/ApolloWrapper.jsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { HttpLink } from "@apollo/client";
+import {
+  ApolloNextAppProvider,
+  ApolloClient,
+  InMemoryCache,
+} from "@apollo/client-integration-nextjs";
+
+function makeClient() {
+  const httpLink = new HttpLink({
+    // this needs to be an absolute url, as relative urls cannot be used in SSR
+    uri: "http://localhost:4000",
+    fetchOptions: {
+      // you can pass additional options that should be passed to `fetch` here,
+      // see https://nextjs.org/docs/app/api-reference/functions/fetch#fetchurl-options
+    },
+    // you can override the default `fetchOptions` on a per query basis
+    // via the `context` property on the options passed as a second argument
+    // to an Apollo Client data fetching hook, e.g.:
+    // const { data } = useSuspenseQuery(MY_QUERY, { context: { fetchOptions: { ... }}});
+  });
+
+  return new ApolloClient({
+    cache: new InMemoryCache(),
+    link: httpLink,
+  });
+}
+
+export function ApolloWrapper({ children }) {
+  return (
+    <ApolloNextAppProvider makeClient={makeClient}>
+      {children}
+    </ApolloNextAppProvider>
+  );
+}
