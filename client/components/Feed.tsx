03784fd454887b11fa07203eebef0296aa283b33
Zach Drummond <68283144+zachdrummond@users.noreply.github.com>
2025-07-07 12:51:35 -0400

    Added all interfaces

---
+++ client/components/Feed.tsx
@@ -0,0 +1,14 @@
+export const Feed = () => {
+
+    const feedToRender = [
+        {
+            id: 1,
+            
+        }
+    ];
+
+    return (
+        <>
+        </>
+    );
+}
--- client/shared/types.d.ts
+++ client/shared/types.d.ts
@@ -1,6 +1,30 @@
+declare interface AuthPayload {
+  token: string
+  author: Author
+}
+
+declare interface Author {
+  author_id: number
+  date_created: string
+  date_updated: string
+  posts: [Post]
+  username: string
+  email: string
+  password: string
+  first_name: string
+  last_name: string
+  role: 'ADMIN' | 'AUTHOR'
+}
+
+declare interface Feed {
+  total: number
+  items: [Post]
+}
+
 declare interface Post {
-  post_id: number,
-  author_id: number,
+  post_id: number
+  author_id: number
+  author: Author
   categories: [string]
   content: string
   date_created: string
