c1963450a1fca092cc0bbb74eb5dc1bd671ebeeb
Zach Drummond <68283144+zachdrummond@users.noreply.github.com>
2025-07-10 16:11:40 -0400

    Added new routes and components

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,11 +1,23 @@
-import { CreatePost } from "@/components/CreatePost";
 import { Feed } from "@/components/Feed";
+import { Header } from "@/components/Header";
 
 export default function Home() {
   return (
-    <>
-      <CreatePost />
-      <Feed />
-    </>
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Feed />
+      </div>
+
+      <div className="center w85">
+        <Header />
+        <div className="ph3 pv1 background-gray">
+          <Routes>
+            <Route path="/" element={<LinkList />} />
+            <Route path="/create" element={<CreateLink />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
   );
 }
