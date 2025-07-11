9b9293263886ec40af24f7e18f7afbc9ea031ebd
Zach Drummond <68283144+zachdrummond@users.noreply.github.com>
2025-07-11 12:09:04 -0400

    Added login to header

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,8 +1,15 @@
+"use client";
+
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { AUTH_TOKEN } from "shared/constants";
 
 export const Header = () => {
+  const authToken = localStorage.getItem(AUTH_TOKEN);
+  const router = useRouter();
+
   return (
-    <header className="flex pal justify-between nowrap orange">
+    <div className="flex pa1 justify-between nowrap orange">
       <div className="flex flex-fixed black">
         <Link href="/" className="no-underline black">
           <div className="fw7 mr1">Zach Drummond's Blog</div>
@@ -11,10 +18,35 @@ export const Header = () => {
           new
         </Link>
         <div className="ml1">|</div>
-        <Link href="/posts/create" className="ml1 no-underline black">
-          submit
+        <Link href="/search" className="ml1 no-underline black">
+          search
         </Link>
+        {authToken && (
+          <div className="flex">
+            <div className="ml1">|</div>
+            <Link href="/posts/create" className="ml1 no-underline black">
+              submit
+            </Link>
+          </div>
+        )}
       </div>
-    </header>
+      <div className="flex flex-fixed">
+        {authToken ? (
+          <div
+            className="ml1 pointer black"
+            onClick={() => {
+              localStorage.removeItem(AUTH_TOKEN);
+              router.push(`/`);
+            }}
+          >
+            logout
+          </div>
+        ) : (
+          <Link href="/admin/login" className="ml1 no-underline black">
+            login
+          </Link>
+        )}
+      </div>
+    </div>
   );
 };
