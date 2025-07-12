3b36eae4b33b4b75c473560b9c2a5f6790c921b6
Zach Drummond <68283144+zachdrummond@users.noreply.github.com>
2025-07-12 10:59:28 -0400

    Added auth to Login

--- client/components/Login.tsx
+++ client/components/Login.tsx
@@ -1,30 +1,113 @@
 "use client";
+import { gql, useMutation } from "@apollo/client";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { AUTH_TOKEN } from "shared/constants";
+
+const SIGNUP_MUTATION = gql`
+  mutation SignupMutation(
+    $email: String!
+    $password: String!
+    $first_name: String!
+    $last_name: String!
+    $username: String!
+  ) {
+    signup(
+      email: $email
+      password: $password
+      first_name: $first_name
+      last_name: $last_name
+      username: $username
+    ) {
+      token
+    }
+  }
+`;
+
+const LOGIN_MUTATION = gql`
+  mutation LoginMutation($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+    }
+  }
+`;
 
 export default function Login() {
   const [form, setForm] = useState({
     login: true,
     email: "",
     password: "",
-    name: ''
+    first_name: "",
+    last_name: "",
+    username: "",
+  });
+  const router = useRouter();
+
+  const [login] = useMutation(LOGIN_MUTATION, {
+    variables: {
+      email: form.email,
+      password: form.password,
+    },
+    onCompleted: ({ login }) => {
+      localStorage.setItem(AUTH_TOKEN, login.token);
+      router.push("/");
+    },
+  });
+
+  const [signup] = useMutation(SIGNUP_MUTATION, {
+    variables: {
+      email: form.email,
+      password: form.password,
+      username: form.username,
+      first_name: form.first_name,
+      last_name: form.last_name,
+    },
+    onCompleted: ({ signup }) => {
+      localStorage.setItem(AUTH_TOKEN, signup.token);
+      router.push("/");
+    },
   });
 
   return (
     <div>
       <h4 className="mv3">{form.login ? "Login" : "Sign Up"}</h4>
       <div className="flex flex-column">
-                {!form.login && (
-          <input
-            value={form.name}
-            onChange={(e) =>
-              setForm({
-                ...form,
-                name: e.target.value
-              })
-            }
-            type="text"
-            placeholder="Your name"
-          />
+        {!form.login && (
+          <>
+            <input
+              value={form.first_name}
+              onChange={(e) =>
+                setForm({
+                  ...form,
+                  first_name: e.target.value,
+                })
+              }
+              type="text"
+              placeholder="Your First Name"
+            />
+            <input
+              value={form.last_name}
+              onChange={(e) =>
+                setForm({
+                  ...form,
+                  last_name: e.target.value,
+                })
+              }
+              type="text"
+              placeholder="Your Last Name"
+            />
+            <input
+              value={form.username}
+              onChange={(e) =>
+                setForm({
+                  ...form,
+                  username: e.target.value,
+                })
+              }
+              type="text"
+              placeholder="Create a username"
+            />
+          </>
         )}
         <input
           value={form.email}
@@ -52,7 +135,7 @@ export default function Login() {
       <div className="flex mt3">
         <button
           className="pointer mr2 button"
-          onClick={() => console.log("onClick")}
+          onClick={() => (form.login ? login : signup)}
         >
           {form.login ? "login" : "create account"}
         </button>
