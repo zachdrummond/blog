79d0b58acc6e559ca93d9712808c91e95502268a
Zach Drummond <68283144+zachdrummond@users.noreply.github.com>
2025-08-21 07:03:31 -0400

    Added types to utils.ts

--- server/src/resolvers/resolvers.ts
+++ server/src/resolvers/resolvers.ts
@@ -1,6 +1,5 @@
 import { mutations } from "./Mutation.js";
 import { queries } from "./Query.js";
-import { formatDate } from "../utils.js";
 import { Resolvers } from "../../shared/types.js";
 
 // GraphQL Resolvers = A collection of functions that fetch the data for the schema
--- server/src/utils.ts
+++ server/src/utils.ts
@@ -1,7 +1,9 @@
 import { jwtVerify, SignJWT } from "jose";
+import { Author } from "../shared/types.js";
+import { IncomingMessage } from "http";
 export const APP_SECRET = "GraphQL-is-aw3some!";
 
-export const createNewToken = async (author) => {
+export const createNewToken = async (author: Author) => {
   return new SignJWT({ author_id: author.author_id, author_role: author.role })
     .setProtectedHeader({ alg: "HS256" })
     .setIssuedAt()
@@ -12,23 +14,23 @@ export const createNewToken = async (author) => {
 /**
  * Formats a date to MM/DD/YYYY format
  */
-export const formatDate = (parent) => {
-  const date_string = parent?.date_created || parent?.date_updated;
-  if (!date_string) return null;
+// export const formatDate = (parent) => {
+//   const date_string = parent?.date_created || parent?.date_updated;
+//   if (!date_string) return null;
 
-  const date = new Date(date_string);
-  return `${date.toLocaleDateString("en-US", {
-    month: "long",
-    day: "numeric",
-    year: "numeric",
-  })} ${date.toLocaleTimeString("en-US", {
-    hour: "numeric",
-    minute: "2-digit",
-    hour12: true,
-  })}`;
-};
+//   const date = new Date(date_string);
+//   return `${date.toLocaleDateString("en-US", {
+//     month: "long",
+//     day: "numeric",
+//     year: "numeric",
+//   })} ${date.toLocaleTimeString("en-US", {
+//     hour: "numeric",
+//     minute: "2-digit",
+//     hour12: true,
+//   })}`;
+// };
 
-export const getAuthor = async (req?, authToken?: string) => {
+export const getAuthor = async (req?: IncomingMessage, authToken?: string) => {
   let payload;
   if (req) {
     const token = req.headers.authorization;
@@ -41,7 +43,7 @@ export const getAuthor = async (req?, authToken?: string) => {
   } else {
     return null;
   }
-  return { id: payload.author_id, role: payload.author_role };
+  return { id: payload?.author_id, role: payload?.author_role };
 };
 
 const getTokenPayload = async (token: string) => {
@@ -55,7 +57,7 @@ const getTokenPayload = async (token: string) => {
   return payload;
 };
 
-export const omitFields = (author) => {
+export const omitFields = (author: Author) => {
   return author?.role === "ADMIN"
     ? { password: true }
     : {
